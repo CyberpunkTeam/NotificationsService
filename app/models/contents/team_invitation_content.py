674b4a8719aeb294fd6ae06592f8d0d56e9456d7
matfonseca <mfonseca@fi.uba.ar>
2022-12-17 19:59:02 -0300

    add logs

--- app/models/contents/team_invitation_content.py
+++ app/models/contents/team_invitation_content.py
@@ -7,9 +7,11 @@ class TeamInvitationContent:
 
     @staticmethod
     def complete(notification):
+        print("entra a completar el contenido de la team invitation")
         team_name = TeamsService.get_name_by_tid(notification.resource_id)
+        print("obtuvo los datos del team")
         user_name = UsersService.get_name_by_uid(notification.sender_id)
-
+        print("obtuvo los datos del user")
         notification.content = TeamInvitationContent.CONTENT.format(
             user_name, team_name
         )
