1639f863642f038220eb1e2a392d98f00d88e22c
matfonseca <mfonseca@fi.uba.ar>
2023-05-27 17:19:29 -0300

    add block and unblock notification

---
+++ app/models/contents/blocker_manager.py
@@ -0,0 +1,30 @@
+class BlockerManager:
+    CONTENT_BLOCK = "Your {} has been blocked"
+    CONTENT_UNBLOCK = "Your {} has been unblocked"
+
+    @staticmethod
+    def complete(notification):
+        metadata = notification.metadata
+        name = metadata.get("name")
+        entity = notification.notification_type
+
+        if entity == "TEAM_BLOCKED":
+            notification.content = BlockerManager.CONTENT_BLOCK.format(f"team {name}")
+        elif entity == "PROJECT_BLOCKED":
+            notification.content = BlockerManager.CONTENT_BLOCK.format(
+                f"project {name}"
+            )
+        elif entity == "CONTENT_BLOCKED":
+            notification.content = BlockerManager.CONTENT_BLOCK.format(
+                f"content {name}"
+            )
+        elif entity == "TEAM_UNBLOCKED":
+            notification.content = BlockerManager.CONTENT_UNBLOCK.format(f"team {name}")
+        elif entity == "PROJECT_UNBLOCKED":
+            notification.content = BlockerManager.CONTENT_UNBLOCK.format(
+                f"project {name}"
+            )
+        elif entity == "CONTENT_UNBLOCKED":
+            notification.content = BlockerManager.CONTENT_UNBLOCK.format(
+                f"content {name}"
+            )
